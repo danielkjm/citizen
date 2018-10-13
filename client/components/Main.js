871ef2f457afa3482bbe0e1e41aa6c06989e12bf
DK <dkdan94@gmail.com>
2018-10-13 18:44:53 -0400

    boilerplate

---
+++ client/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default Main;
